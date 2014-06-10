c762b372d839f309085651061c397693cbf9c36a
Vic Lau <xingyulliiuu@gmail.com>
2014-06-10 15:06:33 +0800

    Illustrate different scoped beans.

---
+++ spring-bean-scopes/src/main/java/org/swordess/toy/spring/beanscope/Data.java
@@ -0,0 +1,15 @@
+package org.swordess.toy.spring.beanscope;
+
+public class Data {
+
+    private Boolean obsolete;
+
+    public void setObsolete(Boolean obsolete) {
+        this.obsolete = obsolete;
+    }
+
+    public Boolean isObsolete() {
+        return obsolete;
+    }
+
+}
