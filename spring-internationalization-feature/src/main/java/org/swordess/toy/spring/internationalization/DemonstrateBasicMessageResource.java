cb4656838fc0398a344f314c4d76e16fdeb39d52
Vic Lau <xingyulliiuu@gmail.com>
2014-06-10 18:13:51 +0800

    Autowire the example bean directly.

--- spring-internationalization-feature/src/main/java/org/swordess/toy/spring/internationalization/DemonstrateBasicMessageResource.java
+++ spring-internationalization-feature/src/main/java/org/swordess/toy/spring/internationalization/DemonstrateBasicMessageResource.java
@@ -3,7 +3,6 @@ package org.swordess.toy.spring.internationalization;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,11 +11,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class DemonstrateBasicMessageResource {
 
     @Autowired
-    private ApplicationContext applicationContext;
+    private Example example;
 
     @Test
     public void demonstrateResourceBundleMessageSource() {
-        Example example = (Example) applicationContext.getBean("example");
         example.execute();
     }
 
